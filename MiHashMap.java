21a950654cffcb26e6e7b30da669eaf17202b248
montyyo <marcoiban@gmail.com>
2015-02-25 13:16:24 +0100

    listas para nombres y numeros

---
+++ MiHashMap.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class MiHashMap
+{
+    //inicializacin de array
+    private String[] name;
+    private int[] numbers;
+    
+    //constructor de array para strings y numeros
+    public MiHashMap()
+    {
+        this.name = new String[0];
+        this.numbers = new int[0];
+    }
+    
+    
+}
